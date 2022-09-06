a477bb088ba84ae444b019c5d52b55399f086160
amirldn <amirmaula@outlook.com>
2022-09-06 11:14:59 +0100

    Added CORS

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
 from backend.runner import pwsh
 from backend.runner.pwshresult import pwshResult
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 app = FastAPI()
+
+# CORS - Cross Origin Resource Sharing
+# Required for the frontend to access the backend
+# https://fastapi.tiangolo.com/tutorial/cors/
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
