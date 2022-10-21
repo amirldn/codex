1cd105f52e33eea8965b5657e85390a399db6d76
amirldn <amirmaula@outlook.com>
2022-10-21 18:15:12 +0100

    created boilerplate new checks

--- backend/api/routers/runner.py
+++ backend/api/routers/runner.py
@@ -9,7 +9,8 @@ router = APIRouter(
 )
 
 
-@router.get("/flagfileexists/", summary="Runs the Test-FlagFileExists pwsh script")
+@router.get("/flagfileexists/",
+            summary="Runs the Test-FlagFileExists pwsh script")
 async def run_flagfileexists():
     result = pwsh.run_and_return("Test-FlagFileExists.ps1", '/Users/amaula/GitHub/codex/backend/tests/flag.txt')
     if 'fault' in result:
@@ -17,7 +18,8 @@ async def run_flagfileexists():
     return result
 
 
-@router.get("/testoutput/", summary="Runs the Test-Output pwsh script")
+@router.get("/testoutput/",
+            summary="Runs the Test-Output pwsh script")
 async def run_test_output():
     result = pwsh.run_and_return("Test-Output.ps1")
     if 'fault' in result:
