1e69747e55e071ee2a9dfb1fe20b06a9b5efcd61
amirldn <amirmaula@outlook.com>
2022-11-09 17:30:43 +0000

    pipenv updated to add celery

--- backend/api/routers/runner.py
+++ backend/api/routers/runner.py
@@ -29,8 +29,23 @@ async def run_test_output():
 
 @router.get("/checkfirewall/",
             summary="Runs the Check-WindowsFirewall check")
-async def run_test_output():
+async def run_check_firewall():
     result = pwsh.run_and_return("Check-WindowsFirewall.ps1")
     if 'fault' in result:
         raise HTTPException(status_code=500, detail=result)
     return result
+
+
+@router.get("/checkguest/",
+            summary="Runs the Check-GuestAccount check")
+async def run_check_guest():
+    result = pwsh.run_and_return("Check-GuestAccount.ps1")
+    if 'fault' in result:
+        raise HTTPException(status_code=500, detail=result)
+    return result
+
+
+@router.post("/runcheck/", summary="Runs a check specified in the request body", status_code=201)
+async def run_check(body: str):
+    check_name = body['check']
+    return JSONResponse(check_name)
