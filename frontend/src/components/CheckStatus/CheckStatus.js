eba2ef779a31654ff5308592db13db22526ee6b6
amirldn <amirmaula@outlook.com>
2022-12-15 21:50:01 +0000

    made overall status depend on the aggregate of all results

--- frontend/src/components/CheckStatus/CheckStatus.js
+++ frontend/src/components/CheckStatus/CheckStatus.js
@@ -4,7 +4,26 @@ import {Button, Card, CardBody, Col, Row} from "reactstrap";
 import 'animate.css';
 import CheckStatusResult from "../CheckStatusResult/CheckStatusResult";
 
+
 export default function CheckStatus({check, taskId}) {
+
+    function GetOverallStatus({results}) {
+        let overallStatus = 'Ok'
+        let textColour = 'text-success'
+        for (let i = 0; i < results.length; i++) {
+            if (results[i].State === 'Crit') {
+                overallStatus = 'Critcal'
+                textColour = 'text-danger'
+                break
+            } else if (results[i].State === 'Warn') {
+                overallStatus = 'Warning'
+                textColour = 'text-warning'
+            }
+        }
+        return (
+            <span className={`statusbar-button-container ${textColour}`}>{overallStatus}</span>
+        )
+    }
 
     const [shouldRefresh, setShouldRefresh] = React.useState(false);
 
@@ -34,57 +53,42 @@ export default function CheckStatus({check, taskId}) {
 
     // If a taskId is present and a status exists, display the status
     if (taskId !== '' && status.task_status) {
-        // console.log("taskID for " + check.api_name + " is " + taskId.task_id)
         if (status.task_status === 'PENDING') {
-            return (
-                <div>
-                    <p>Status: <b><span
-                        className="text-info animate__animated animate__pulse">{status.task_status}</span></b></p>
-                </div>
-            )
+            return (<div>
+                <p>Status: <b><span
+                    className="text-info animate__animated animate__pulse">{status.task_status}</span></b></p>
+            </div>)
         }
 
 
         if (status.task_status === 'SUCCESS') {
-            return (
-                <div>
-                    <p>Status: <b><span
-                        className="text-success animate__animated animate__pulse">{status.task_status}</span></b></p>
-                    <p><b>Result:</b></p>
-                        <br/>
-                        {status.task_result.data.map((item) => (
-                            <CheckStatusResult props={item} key={item.ID}/>
-                            // <li key={item.ID}>
-                            //     <b>{item.CheckName}</b>
-                            //     <br/>
-                            //     {item.Message}
-                            //     <br/>
-                            //     {item.State}
-                            // </li>
-                        ))}
-                </div>
-            )
+            return (<div>
+                <p>Status: <b>
+                        <span className="text-success animate__animated animate__pulse">
+                        <GetOverallStatus results={status.task_result.data}/>
+                        </span
+                        ></b></p>
+                <p><b>Result:</b></p>
+                <br/>
+                {status.task_result.data.map((item) => (<CheckStatusResult props={item} key={item.ID}/>))}
+            </div>)
         }
-
-
     }
 
 
     // If there was an error
     if (taskId !== '' && status.detail) {
-        return (
-            <div>
-                <p>Status: <b><span
-                    className="text-danger animate__animated animate__pulse">Unknown - Internal Error</span></b></p>
-                <p><b>Error Detail:</b>
-                    <br/>
-                    {status.detail.fault.brief}
-                    <br/>
-                    <br/>
-                    <i>{status.detail.fault.stderr}</i>
-                </p>
-            </div>
-        )
+        return (<div>
+            <p>Status: <b><span
+                className="text-danger animate__animated animate__pulse">Unknown - Internal Error</span></b></p>
+            <p><b>Error Detail:</b>
+                <br/>
+                {status.detail.fault.brief}
+                <br/>
+                <br/>
+                <i>{status.detail.fault.stderr}</i>
+            </p>
+        </div>)
     }
 
 
@@ -97,10 +101,8 @@ export default function CheckStatus({check, taskId}) {
 
 
     // If no status is set
-    return (
-        <div className="animate__animated animate__fadeInUp rounded pt-1 ">
-            <p>Status: <b><span className="text-secondary">Not Run</span></b></p>
-        </div>
-    )
+    return (<div className="animate__animated animate__fadeInUp rounded pt-1 ">
+        <p>Status: <b><span className="text-secondary">Not Run</span></b></p>
+    </div>)
 }
 
