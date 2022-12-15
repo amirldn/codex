a17fb817da4fd1fe74a2a64da8f290be5a1961c8
amirldn <amirmaula@outlook.com>
2022-12-15 20:55:34 +0000

    meh doesnt really work well

--- frontend/src/components/CheckStatusResult/CheckStatusResult.js
+++ frontend/src/components/CheckStatusResult/CheckStatusResult.js
@@ -1,7 +1,18 @@
 import React, {useState} from "react";
-import {Button, Card, CardBody, CardHeader, Col, Collapse, Row} from "reactstrap";
+import {Button, Card, CardBody, CardHeader, Col, Collapse, Container, Row} from "reactstrap";
 
 import 'animate.css';
+
+function DrawStatusIcon(props) {
+    if (props.status === 'Ok') {
+        return <i className="fa fa-check-circle fa-3x text-success"/>
+    } else if (props.status === 'Warn') {
+        return <i className="fa fa-exclamation-circle fa-3x text-warning"/>
+    } else if (props.status === 'Crit') {
+        return <i className="fa fa-times-circle fa-3x text-danger"/>
+    }
+    return <i className="fa fa-question-circle fa-3x text-info"/>
+}
 
 export default function CheckStatusResult(props) {
 
@@ -13,37 +24,29 @@ export default function CheckStatusResult(props) {
         <div className="animate__animated animate__fadeInUp rounded p-1">
             <Card className="card-check m-1">
                 <CardHeader>
-                    <Button className="btn btn-link rounded"
-                            onClick={toggle}>
-                        <Row>
-                            <Col md="8">
-                                <Row>
-                                    <Col>
-                                        <Row>
-                                            <Col md="4">
-                                                <b><i className='fa fa-user'/></b>
-                                            </Col>
-                                            <Col md="8">
-                                                {/*{console.log(props)}*/}
-                                                <b> {props.props.CheckName}</b>
-                                            </Col>
-                                        </Row>
-                                    </Col>
-                                </Row>
-                            </Col>
-                            <Col md="2">
-                                <i className="fa fa-check-circle fa-5x text-success"/>
-                            </Col>
-                            <Col md="2">
-                             <i className="fa fa-chevron-down"/>
-                            </Col>
-                        </Row>
-                    </Button>
+                    <Container className="d-flex justify-content-between">
+                        <Button className="btn btn-link rounded"
+                                onClick={toggle} >
+                            <Row >
+                                <Col md="1">
+                                    <DrawStatusIcon status={props.props.State}/>
+                                </Col>
+                                <Col md="10">
+                                    <b>{props.props.CheckName}</b>
+                                </Col>
+                                <Col md="1">
+                                    <i className="fa fa-chevron-down"/>
+                                </Col>
+                            </Row>
+                        </Button>
+                    </Container>
                 </CardHeader>
                 <Collapse isOpen={isOpen}>
                     <CardBody>
                         <Row>
-                            <i>{props.props.Message}</i>
+                            <Col>
+                                <i>{props.props.Message}</i>
+                            </Col>
                         </Row>
                     </CardBody>
                 </Collapse>
