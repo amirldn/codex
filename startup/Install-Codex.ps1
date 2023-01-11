32365f6dc54d06bc6e9f4d415112930505b44ef9
amirldn <amirmaula@outlook.com>
2023-01-11 22:37:58 +0000

    add startup script

---
+++ startup/Install-Codex.ps1
@@ -0,0 +1,12 @@
+# This script will install all of the prerequisites for the Codex project.
+# It will also install the Codex project itself.
+
+# Will need to check the current version of Powershell
+# If not PowerShell 7, begin installing
+
+# Install Python 3.10 (or extract some built version of it?)
+# Install NodeJS 16.13.0 (or extract some built version of it?)
+
+# Install the Codex project
+
+# Create shortcuts to the start script for the Codex project
