e7b6e0e4b7100b0108117ebac920f97e9ca1ee2b
amirldn <amirmaula@outlook.com>
2023-01-11 23:34:41 +0000

    Windows now runs the test-output and imports Codex module successfully

--- backend/checks/Test-Output.ps1
+++ backend/checks/Test-Output.ps1
@@ -2,11 +2,9 @@
 
 # Import the Codex module
 $modulePath = Join-Path (Get-Location) "backend" -AdditionalChildPath @("checks","Codex","Codex.psm1")
-Write-Output "Module path: $modulePath"
 Import-Module $modulePath -Force
 
 # Add some Codex output and display it
-
 Add-CodexOutput -CheckName "Test_Ok" -State Ok -Message "This is OK test output"
 Add-CodexOutput -CheckName "Test_Warn" -State Warn -Message "This is WARN test output"
 Add-CodexOutput -CheckName "Test_Crit" -State Crit -Message "This is CRIT test output"
