46334e3bf3b896e0a3d31d7b64529e7b6b19745a
amirldn <amirmaula@outlook.com>
2023-01-27 00:49:09 +0000

    Added generating logic

--- backend/api/router/passwordgen.py
+++ backend/api/router/passwordgen.py
@@ -1,8 +1,8 @@
 import logging
 import random
+import time
 
 from fastapi import APIRouter
-
 
 router = APIRouter(
     prefix="/password",
@@ -14,29 +14,33 @@ async def gen_password(length: int = 12, special: bool = True, numbers: bool = T
     lower = "abcdefghijklmnopqrstuvwxyz"
     upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     special_chars = '!@#$%^&*()'
-    numbers = '0123456789'
-    types = [lower, upper, numbers, special_chars]
+    number_chars = '0123456789'
+    types = [lower, upper, number_chars, special_chars]
 
     logging.info("Generating password of length: {}, special: {}, numbers:{}".format(length, special, numbers))
 
     password = ""
-    types_included = []
-    all_types_included = False
     for i in range(length):
         if special and numbers:
-            next = random.choice(types)
+            next_char_type = random.randint(0, len(types) - 1)
+            next = types[next_char_type]
         elif special:
-            next = random.choice(types[:3])
+            next_char_type = random.randint(0, len(types) - 2)
+            next = types[next_char_type]
         elif numbers:
-            next = random.choice(types[:2])
+            next_char_type = random.randint(0, len(types) - 3)
+            next = types[next_char_type]
         else:
-            next = random.choice(types[:1])
-        if next not in types_included:
-            types_included += next
+            next_char_type = random.randint(0, len(types) - 4)
+            next = types[next_char_type]
+
         length_of_char_type_array = len(next)
-        password += (next[random.randint(0, length_of_char_type_array-1)])
-        print(password)
+        char = next[random.randint(0, length_of_char_type_array - 1)]
+        password += char
 
     return {"data": {
         "password": password,
     }}
+
+
+
