0a581aee2fb62429e80d3f63446291666f411d5f
amirldn <amirmaula@outlook.com>
2023-02-01 19:48:47 +0000

    Added defender check

--- backend/checks/Check-WindowsDefender.ps1
+++ backend/checks/Check-WindowsDefender.ps1
@@ -1,3 +1,14 @@
 # Import the Codex module
 $modulePath = Join-Path (Get-Location) "backend" -AdditionalChildPath @("checks","Codex","Codex.psm1")
 Import-Module $modulePath -Force
+
+#Check if Windows Defender is enabled
+$defender = Get-MpPreference
+if ($defender.DisableRealtimeMonitoring -eq $false) {
+    Add-CodexOutput -CheckName "Windows Defender" -State Ok -Message "Windows Defender is enabled"
+} else {
+    Add-CodexOutput -CheckName "Windows Defender" -State Crit -Message "Windows Defender is disabled"
+}
+
+
+Write-CodexOutput
