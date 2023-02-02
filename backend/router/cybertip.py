8d50dd9b358555a2548e2e6e83e8fa70ebe9b84d
amirldn <amirmaula@outlook.com>
2023-02-02 19:16:25 +0000

    Moved cybertip

--- backend/router/cybertip.py
+++ backend/router/cybertip.py
@@ -1,21 +1,17 @@
 import json
 import logging
 import random
-from pathlib import Path
-
+import os
 from fastapi import APIRouter
 
-# Open the json file of cyber security tips
-# get the absolute path of ../../cybertip/cybertip.json using pathlib
-tip_file = Path(__file__).parent.parent / "cybertip/cybertip.json"
+cybertip = os.path.abspath('router/cybertip.json')
 
-with open(tip_file) as f:
+with open(cybertip) as f:
     tips = (json.loads(f.read()))['tips']
 
 router = APIRouter(
     prefix="/cybertip",
 )
-
 
 @router.get("/tip/", summary="Get a random cyber security tip")
 async def get_cybertip():
