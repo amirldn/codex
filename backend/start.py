56cb30e1c3a90b9c5e2c3d23ddb14ee6d3c3aba7
amirldn <amirmaula@outlook.com>
2023-02-15 18:51:44 +0000

    Got Celery & Uvicorn working from start.py in threaded

--- backend/start.py
+++ backend/start.py
@@ -1,14 +1,41 @@
 import multiprocessing
 import uvicorn
+import threading
 from backend.runner.tasks import celeryi
 
+exitFlag = 0
 
-# TODO: add celery
-# https://stackoverflow.com/questions/23389104/how-to-start-a-celery-worker-from-a-script-module-main
-if __name__ == '__main__':
-    multiprocessing.freeze_support()
+
+class thread(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        print("Starting " + self.name)
+        if self.name == 'uvicornThread':
+            start_uvicorn()
+        elif self.name == 'celeryThread':
+            start_celery()
+        print("Exiting " + self.name)
+
+
+def start_uvicorn():
     uvicorn.run("backend.main:app", host="127.0.0.1", port=8000, reload=False, workers=2)
+
+
+def start_celery():
     worker = celeryi.Worker(
         include=['backend.runner.tasks']
     )
     worker.start()
+
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    celeryThread = thread(2, "celeryThread", 2)
+    celeryThread.start()
+
+    start_uvicorn()
