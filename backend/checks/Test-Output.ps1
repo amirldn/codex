b422cc11a2074dce8629a18814f4f72e68091ea5
amirldn <amirmaula@outlook.com>
2023-02-15 15:34:06 +0000

    Celery tasks now get made, need a way to run celery too

--- backend/checks/Test-Output.ps1
+++ backend/checks/Test-Output.ps1
@@ -2,19 +2,7 @@
 
 # Import the Codex module
 $pwd = $MyInvocation.MyCommand.Path
-#"/var/folders/rk/gm7_jnjn6cxbl9615x6k5jfh0000gn/T/_MEIo76i0F/backend/checks/Test-Output.ps1"
-# Replace the end of the path with the Codex module path
-
 $modulePath = Join-Path ($pwd -replace '[^\\/]+$','') "Codex" -AdditionalChildPath @("Codex.psm1")
-
-#if ($pwd -notlike "*backend*")
-#{
-#    $modulePath = Join-Path (Get-Location) "backend" -AdditionalChildPath @("checks","Codex","Codex.psm1")
-#}
-#else
-#{
-#    $modulePath = Join-Path (Get-Location) "checks" -AdditionalChildPath @("Codex","Codex.psm1")
-#}
 Import-Module $modulePath -Force
 
 # Add some Codex output and display it
