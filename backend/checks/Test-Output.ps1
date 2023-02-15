9f81b567bc64fe414cfc71722cec5df27b60f3cb
amirldn <amirmaula@outlook.com>
2023-02-15 15:23:48 +0000

    Think I fixed module importing for test-output

--- backend/checks/Test-Output.ps1
+++ backend/checks/Test-Output.ps1
@@ -1,15 +1,20 @@
 # This check returns a simple value back as stdout. Used as a test file to ensure things are set up correctly.
 
 # Import the Codex module
-$pwd = Get-Location
-if ($pwd -notlike "*backend*")
-{
-    $modulePath = Join-Path (Get-Location) "backend" -AdditionalChildPath @("checks","Codex","Codex.psm1")
-}
-else
-{
-    $modulePath = Join-Path (Get-Location) "checks" -AdditionalChildPath @("Codex","Codex.psm1")
-}
+$pwd = $MyInvocation.MyCommand.Path
+#"/var/folders/rk/gm7_jnjn6cxbl9615x6k5jfh0000gn/T/_MEIo76i0F/backend/checks/Test-Output.ps1"
+# Replace the end of the path with the Codex module path
+
+$modulePath = Join-Path ($pwd -replace '[^\\/]+$','') "Codex" -AdditionalChildPath @("Codex.psm1")
+
+#if ($pwd -notlike "*backend*")
+#{
+#    $modulePath = Join-Path (Get-Location) "backend" -AdditionalChildPath @("checks","Codex","Codex.psm1")
+#}
+#else
+#{
+#    $modulePath = Join-Path (Get-Location) "checks" -AdditionalChildPath @("Codex","Codex.psm1")
+#}
 Import-Module $modulePath -Force
 
 # Add some Codex output and display it
