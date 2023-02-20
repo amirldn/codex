5e69828b86b765541926f69ff3780d3a56bcc6bf
amirldn <amirmaula@outlook.com>
2023-02-20 23:36:44 +0000

    added function to getLatest but not used yet

--- frontend/src/components/CheckCard/CheckCard.js
+++ frontend/src/components/CheckCard/CheckCard.js
@@ -5,15 +5,12 @@ import 'animate.css';
 import CheckStatus from "../CheckStatus/CheckStatus";
 
 export default function CheckCard(props) {
-    //  Write a function that will submit a post request to run the check
-
     const [taskId, setTaskId] = React.useState('');
 
     function RunCheck(api_name) {
         // Send the data then store the response
         fetch(("http://127.0.0.1:8000/check/run/?check_name=" + api_name), {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
+            method: "POST", headers: {"Content-Type": "application/json"},
         })
             .then(response => response.json())
             .then(data => {
@@ -26,52 +23,52 @@ export default function CheckCard(props) {
     }
 
     const TaskIDContext = React.createContext({
-        taskId: [], fetchTaskId: () => {
+        taskId: '', fetchTaskId: () => {
         }
     })
 
+    // TODO: On creation of the component, fetch the latest task id for the check
+    // TODO: Maybe delete TaskIDContext - don't think it's needed
 
-    return (
-        <TaskIDContext.Provider value={{taskId, fetchTaskId}}>
-            <div className="animate__animated animate__fadeInUp rounded p-1">
-                <Card className="card-check m-1">
-                    <CardBody>
-                        <h5><i className={props.check.icon}/> {props.check.friendly_name}</h5>
-                        <i>{props.check.description}</i>
-                        <Row>
-                            <Col md="6">
-                                <p><b>Last Run:</b>
-                                    <br/>
-                                    5:45pm @ 11 Dec 2022
-                                </p>
-                            </Col>
-                            <Col md="6">
-                                <Button className="btn-round" color="success" outline
-                                        onClick={() => RunCheck(props.check.api_name)}>
-                                    <i className="nc-icon nc-check-2"/> Run Check
-                                </Button>
-                            </Col>
-                        </Row>
-                        <CheckStatus
-                            check={props.check}
-                            taskId={taskId}
-                        />
+    function getLatest(api_name) {
+        // Send the data then store the response
+        fetch(("http://127.0.0.1:8000/check/id/latest/?check_name=" + api_name), {
+            method: "GET", headers: {"Content-Type": "application/json"},
+        })
+            .then(response => response.json())
+            .then(data => {
+                // console.log(data)  // Print the data
+                setTaskId(data);
+            })
+    }
 
-                        {/*<Row>*/}
-                        {/*    <Col md="6">*/}
-                        {/*        <Button className="btn-round" color="info" outline>*/}
-                        {/*            <i className="nc-icon nc-refresh-69"/> Refresh*/}
-                        {/*        </Button>*/}
-                        {/*    </Col>*/}
-                        {/*</Row>*/}
 
-                    </CardBody>
-                </Card>
-                {/*<div className="rounded bg-secondary text-white">*/}
-                {/*  <p>hello</p>*/}
-                {/*</div>*/}
-            </div>
-        </TaskIDContext.Provider>
-    )
+    return (<TaskIDContext.Provider value={{taskId, fetchTaskId}}>
+        <div className="animate__animated animate__fadeInUp rounded p-1">
+            <Card className="card-check m-1">
+                <CardBody>
+                    <h5><i className={props.check.icon}/> {props.check.friendly_name}</h5>
+                    <i>{props.check.description}</i>
+                    <Row>
+                        <Col md="6">
+                            <p><b>Last Run:</b>
+                                <br/>
+                                5:45pm @ 11 Dec 2022
+                            </p>
+                        </Col>
+                        <Col md="6">
+                            <Button className="btn-round" color="success" outline
+                                    onClick={() => RunCheck(props.check.api_name)}>
+                                <i className="nc-icon nc-check-2"/> Run Check
+                            </Button>
+                        </Col>
+                    </Row>
+                    <CheckStatus
+                        check={props.check}
+                        taskId={taskId}
+                    />
+                </CardBody>
+            </Card>
+        </div>
+    </TaskIDContext.Provider>);
 }
-
