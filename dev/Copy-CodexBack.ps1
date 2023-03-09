b1ec6614ea870cdcc2622b5c6013c1b9c329283b
amirldn <amirmaula@outlook.com>
2023-03-09 14:04:50 +0000

    Add checks for frontend and backend so it quits after and doesnt double launch

--- dev/Copy-CodexBack.ps1
+++ dev/Copy-CodexBack.ps1
@@ -1,9 +1,18 @@
 # Delete any existing files in the backend directory
 Remove-Item -Path '/Volumes/`[C`] Windows 11 (ARM)/codex-windows/backend' -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path '/Volumes/`[C`] Windows 11 (ARM)/codex-windows/startup' -Recurse -Force -ErrorAction SilentlyContinue
 
 # Copy the codex-windows directory from the current directory to C:/
 $filenames = Get-ChildItem '/Users/amaula/GitHub/codex/backend' | Select-Object name
 foreach($filename in $filenames) {
     Write-Output $filename.name
     Copy-Item -Path ('/Users/amaula/GitHub/codex/backend/' + $filename.name) -Destination ("/Volumes/`[C`] Windows 11 (ARM)/codex-windows/backend") -Recurse -Force -ErrorAction Continue
+}
+
+
+
+$filenames = Get-ChildItem '/Users/amaula/GitHub/codex/startup' | Select-Object name
+foreach($filename in $filenames) {
+    Write-Output $filename.name
+    Copy-Item -Path ('/Users/amaula/GitHub/codex/startup/' + $filename.name) -Destination ("/Volumes/`[C`] Windows 11 (ARM)/codex-windows/startup") -Recurse -Force -ErrorAction Continue
 }
--- startup/Start-Codex.ps1
+++ startup/Start-Codex.ps1
@@ -3,31 +3,62 @@ $codexBackEndPath = "C:\Program Files (x86)\Codex\codex-io-backend.exe"
 $redisPath = "C:\Program Files\Memurai\memurai.exe"
 
 # Check Redis is running
-if (-not (Test-Path $redisPath))
+if (-not(Test-Path $redisPath))
 {
-    Write-Error "Memurai (Redis) path does not exist. Please double check the path in the Codex startup script."
+    Write-Error "Memurai (Redis) path does not exist. Please double check that it is installed."
     exit 1
 }
 
 $redisProcess = Get-Process -Name redis-server -ErrorAction SilentlyContinue
-if (-not $redisProcess)
+if (-not$redisProcess)
 {
     Start-Process -FilePath $redisPath
 }
 
 # Start the Codex backend
-if (-not (Test-Path $codexBackEndPath))
+if (-not(Test-Path $codexBackEndPath))
 {
     Write-Error "Codex backend path does not exist. Please double check the path in the Codex startup script."
-#    exit 1
+    exit 1
+}
+
+$backendProcess = Get-Process -Name codex-io-backend -ErrorAction SilentlyContinue
+if ($backendProcess)
+{
+    foreach ($process in $backendProcess)
+    {
+        $process | Stop-Process -Force -ErrorAction SilentlyContinue
+    }
 }
 Start-Process -FilePath $codexBackEndPath -WindowStyle Hidden
 
 # Start the Codex frontend
-if (-not (Test-Path $codexFrontEndPath))
+if (-not(Test-Path $codexFrontEndPath))
 {
     Write-Error "Codex frontend path does not exist. Please double check the path in the Codex startup script."
-#    exit 1
+    exit 1
 }
-Start-Process -FilePath $codexFrontEndPath
 
+$frontendProcess = Get-Process -Name codex-io -ErrorAction SilentlyContinue
+if (-not$frontendProcess)
+{
+    Start-Process -FilePath $codexFrontEndPath
+}
+else
+{
+    Write-Warning "It looks like Codex is already running? Check your taskbar for the Codex icon."
+    exit 1
+}
+
+# Wait until Codex frontend is no longer running then stop the backend
+$frontendProcess = Get-Process -Name codex-io -ErrorAction SilentlyContinue
+while ($frontendProcess)
+{
+    Start-Sleep -Seconds 10
+    $frontendProcess = Get-Process -Name codex-io -ErrorAction SilentlyContinue
+}
+$backendProcess = Get-Process -Name codex-io-backend -ErrorAction SilentlyContinue
+foreach ($process in $backendProcess)
+{
+    $process | Stop-Process -Force -ErrorAction SilentlyContinue
+}
