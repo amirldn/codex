16f21b22b334113be40bffb30166f802f498dc7f
amirldn <amirmaula@outlook.com>
2023-03-15 17:31:48 +0000

    Got apply fix working

--- backend/checks/Fix-TestOutput.ps1
+++ backend/checks/Fix-TestOutput.ps1
@@ -10,8 +10,8 @@ $pwd = $MyInvocation.MyCommand.Path
 $modulePath = Join-Path ($pwd -replace '[^\\/]+$', '') "Codex" -AdditionalChildPath @("Codex.psm1")
 Import-Module $modulePath -Force
 
-#Add-CodexOutput -CheckName 'Failed' -State Crit -Message 'Failed to disable the Guest account.'
+Add-CodexOutput -CheckName 'Failed' -State Crit -Message 'Failed to disable the Guest account.'
 
-Add-CodexOutput -CheckName 'Guest is Disabled' -State Ok -Message "Guest account is now disabled."
+#Add-CodexOutput -CheckName 'Guest is Disabled' -State Ok -Message "Guest account is now disabled."
 
 Write-CodexOutput
