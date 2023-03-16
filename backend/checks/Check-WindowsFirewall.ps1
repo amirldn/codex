d49a6d83301561082185c2fefd705972e3b95e53
amirldn <amirmaula@outlook.com>
2023-03-16 15:37:02 +0000

    Added steps to resolve Windows Firewall

--- backend/checks/Check-WindowsFirewall.ps1
+++ backend/checks/Check-WindowsFirewall.ps1
@@ -7,16 +7,34 @@ $ErrorActionPreference = 'Stop'
 
 # Import the Codex module
 $pwd = $MyInvocation.MyCommand.Path
-$modulePath = Join-Path ($pwd -replace '[^\\/]+$','') "Codex" -AdditionalChildPath @("Codex.psm1")
+$modulePath = Join-Path ($pwd -replace '[^\\/]+$', '') "Codex" -AdditionalChildPath @("Codex.psm1")
 Import-Module $modulePath -Force
 
 $firewall = Get-NetFirewallProfile
 $firewall | ForEach-Object {
-    if ($_.Enabled -eq 'True') {
-        Add-CodexOutput -CheckName "$($_.Name) Firewall Disabled" -State Ok -Message "$(($_.Name) + ' is enabled')"
+    if ($_.Enabled -eq 'True')
+    {
+        $params = @{
+            CheckName = "$( $_.Name ) Firewall is Enabled"
+            State = 'Ok'
+            Message = "The firewall for the profile '$( $_.Name )' is enabled - you should keep it enabled to protect your system from malicious traffic (e.g. intrusion attempts, ransomware attacks, etc.)."
+        }
+        Add-CodexOutput @params
     }
-    else {
-        Add-CodexOutput -CheckName "$($_.Name) Firewall Disabled" -State Crit -Message "The firewall for the profile '$($_.Name)' is disabled - you should enable it to protect your system from malicious traffic (e.g. intrusion attempts, ransomware attacks, etc.)."
+    else
+    {
+        $params = @{
+            CheckName = "$( $_.Name ) Firewall Disabled"
+            State = 'Warn'
+            Message = "The firewall for the profile '$( $_.Name )' is disabled - you should enable it to protect your system from malicious traffic (e.g. intrusion attempts, ransomware attacks, etc.)."
+            ResolveSteps = @("Search for 'Windows Firewall' from your start menu and launch the 'Windows Defender Firewall with Advanced Security'."
+            ,"Click on 'Windows Defender Firewall Properties'.",
+            "For each profile, in this case, $( $_.Name ), click on the 'Firewall State' dropdown and select 'On'.",
+            "Click 'OK' to save your changes.")
+            ResolveImg = @("4qriKO1.png", "WHNeBk9.png", "YXYUWVy.png", "kiJA0V5.png")
+            ResolveScript = 'fixwindowsfirewall'
+        }
+        Add-CodexOutput @params
     }
 }
 
